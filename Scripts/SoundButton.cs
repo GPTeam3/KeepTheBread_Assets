1e57a87943dd644c2e8bfd2ae29b998fc24bb179
joaga0 <joaga0@naver.com>
2023-10-23 11:56:18 +0900

    sound

---
+++ Scripts/SoundButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class SoundButton : MonoBehaviour
+{
+    public GameObject obj;
+    public AudioSource gamesound;
+
+    void Start()
+    {
+        gamesound = obj.GetComponent<AudioSource>();
+        gamesound.Play();
+
+        gamesound.volume = 1.0f;
+        gamesound.loop = true;
+    }
+
+    public void SoundOn()
+    {
+        gamesound.volume = 1.0f;
+    }
+
+    public void SoundOff()
+    {
+        gamesound.volume = 0.0f;
+    }
+}
